a18082d7807f78a1029ec6fe0ac1e1aea103cf38
ffFamous <ingvar.kokorin@gmail.com>
2020-12-28 02:22:09 +0300

    Commit 1

--- src/SystemIn.java
+++ src/SystemIn.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class Years {
+public class SystemIn {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String name = reader.readLine();
